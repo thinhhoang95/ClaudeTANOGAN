a7f83369bd5e829ff6dab987b4e9b77ce6b0134c
Thinh Hoang <thinhhoangdinh95@hotmail.com>
2024-07-10 15:46:17 +0200

    Training of VAE

--- pod.sh
+++ pod.sh
@@ -1,2 +1,11 @@
 echo "Installing dependencies..."
 pip install tqdm scikit-learn pandas matplotlib numpy
+
+echo "Setting up Git..."
+git config --global user.email "thinhhoangdinh95@hotmail.com"
+git config --global user.name "Thinh Hoang Dinh"
+
+# Changing directory to the project root
+#cd /workspace
+#git clone https://github.com/thinhhoang95/ClaudeTANOGAN.git
+
